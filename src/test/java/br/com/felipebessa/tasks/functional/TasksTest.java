1a131729e9d91db2caf11dd960e1c77c81f253e5
Felipe Bessa <ffvechio@gmail.com>
2020-05-08 14:52:11 -0300

    ajuste no endereço da URL do selenium grid

--- src/test/java/br/com/felipebessa/tasks/functional/TasksTest.java
+++ src/test/java/br/com/felipebessa/tasks/functional/TasksTest.java
@@ -21,10 +21,10 @@ public class TasksTest {
     Faker faker = new Faker(new Locale("pt-BR"));
 
     public WebDriver setup() throws MalformedURLException {
-        WebDriver driver = new ChromeDriver();
-//        DesiredCapabilities cap = DesiredCapabilities.chrome();
-//        WebDriver driver = new RemoteWebDriver(new URL("http://192.168.15.21:4444/wd/hub"), cap);
-        driver.navigate().to("http://localhost:8001/tasks");
+//        WebDriver driver = new ChromeDriver();
+        DesiredCapabilities cap = DesiredCapabilities.chrome();
+        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
+        driver.navigate().to("http://192.168.15.14:8001/tasks");
         driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
         return driver;
     }
